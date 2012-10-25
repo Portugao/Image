f7e4e29d8d9fd033e1ed22678cc1ecb8e3d8345b
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-10-25 10:34:28 +0200

    get repositories

--- src/modules/MUImage/lib/MUImage/Util/Model.php
+++ src/modules/MUImage/lib/MUImage/Util/Model.php
@@ -16,5 +16,34 @@
  */
 class MUImage_Util_Model extends MUImage_Util_Base_Model
 {
-
+    
+    /**
+	*
+	 This method is for getting a repository for pictures
+	*
+	*/
+    
+    public static function getPictureRepository() {
+    
+     $serviceManager = ServiceUtil::getManager();
+     $entityManager = $serviceManager->getService('doctrine.entitymanager');
+     $repository = $entityManager->getRepository('MUImage_Entity_Picture');
+    
+     return $repository;
+    }
+    
+    /**
+	*
+	 This method is for getting a repository for albums
+	*
+	*/
+    
+    public static function getAlbumRepository() {
+    
+     $serviceManager = ServiceUtil::getManager();
+     $entityManager = $serviceManager->getService('doctrine.entitymanager');
+     $repository = $entityManager->getRepository('MUImage_Entity_Album');
+    
+     return $repository;
+    } 
 }
