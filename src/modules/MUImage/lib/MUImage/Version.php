80486616638c2925defd8d5b8b764f6b179b3e2f
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-11-01 15:36:48 +0100

    block permissions

--- src/modules/MUImage/lib/MUImage/Version.php
+++ src/modules/MUImage/lib/MUImage/Version.php
@@ -16,5 +16,49 @@
  */
 class MUImage_Version extends MUImage_Base_Version
 {
-    // custom enhancements can go here
+
+    public function getMetaData()
+    {
+        $meta = array();
+        // the current module version
+        $meta['version'] = '1.0.0';
+        // the displayed name of the module
+        $meta['displayname'] = $this->__('MUImage');
+        // the module description
+        $meta['description'] = $this->__('MUImage Pictures in Albums and SubAlbums');
+        //! url version of name, should be in lowercase without space
+        $meta['url'] = $this->__('muimage');
+        // core requirement
+        $meta['core_min'] = '1.3.1'; // requires minimum 1.3.1 or later
+        $meta['core_max'] = '1.3.99'; // not ready for 1.4.0 yet
+
+        // define special capabilities of this module
+        $meta['capabilities'] = array(
+            HookUtil::SUBSCRIBER_CAPABLE => array('enabled' => true) /*,
+             HookUtil::PROVIDER_CAPABLE => array('enabled' => true), // TODO: see #15
+             'authentication' => array('version' => '1.0'),
+             'profile'        => array('version' => '1.0', 'anotherkey' => 'anothervalue'),
+             'message'        => array('version' => '1.0', 'anotherkey' => 'anothervalue')
+             */
+        );
+
+        // permission schema
+        // DEBUG: permission schema aspect starts
+        $meta['securityschema'] = array(
+            'MUImage::'             => '::',
+
+            'MUImage:Album:Album'   => 'AlbumID:AlbumID:',
+
+            'MUImage:Album:'        => 'AlbumID::',
+            'MUImage:Album:Picture' => 'AlbumID:PictureID:',
+
+            'MUImage:Picture:'      => 'PictureID::',
+            
+            'MUImage:OneItemBlock:'      => 'BlockId::',
+            'MUImage:ItemListBlock:'      => 'BlockId::'
+        );
+        // DEBUG: permission schema aspect ends
+
+        return $meta;
+    }
 }
