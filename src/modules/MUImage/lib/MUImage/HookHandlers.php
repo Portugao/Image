2a9234c3216e3676d74e3a33fdebd2f8c6114ce4
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-12-25 19:29:09 +0100

    Possibilty to select existing albums

---
+++ src/modules/MUImage/lib/MUImage/HookHandlers.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * MUImage.
+ *
+ * @copyright Michael Ueberschaer
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @package MUImage
+ * @author Michael Ueberschaer <kontakt@webdesign-in-bremen.com>.
+ * @link http://www.webdesign-in-bremen.com
+ * @link http://zikula.org
+ * @version at Son Dec 23
+ */
+
+/**
+ * HookHandler class
+ */
+class MUImage_HookHandlers extends Zikula_Hook_AbstractHandler
+{
+
+	/**
+	 * Zikula_View instance
+	 * @var object
+	 */
+	private $view;
+
+	/**
+	 * Post constructor hook.
+	 */
+	public function setup()
+	{
+		$this->view = Zikula_View::getInstance("MUImage");
+	}
+
+	/**
+	 * Display a checkbox for choose to create an album or not
+	 *
+	 * @param Zikula_DisplayHook $hook
+	 */
+	public function uiEdit(Zikula_DisplayHook $hook)
+	{
+
+		// some initialization stuff
+		$assign = (isset($params['assign']) && !empty($params['assign'])) ? $params['assign'] : 'smilies';
+		$type = (isset($params['type']) && !empty($params['type'])) ? $params['type'] : '';
+
+		$allowedFields = MUImage_Util_Controller::allowedFields();
+
+		$albumrepository = MUImage_Util_Model::getAlbumRepository();
+		$where = 'tbl.parent_id IS NULL';
+		$albums = $albumrepository->selectWhere($where);
+
+		$where2 = 'tbl.parent_id IS NOT NULL';
+		$subalbums = $albumrepository->selectWhere($where2);
+
+		// Assign the number of allowed fields to template
+		$this->view->assign('allowedFields', $allowedFields);
+		$this->view->assign('albums', $albums);
+		$this->view->assign('subalbums', $subalbums);
+
+		$hook->setResponse(new Zikula_Response_DisplayHook('provider.muimage.ui_hooks.service', $this->view, 'hook/edit.tpl'));
+	}
+
+	/**
+	 * Display smilies and provide interface for their use in an edit object form
+	 *
+	 * @param Zikula_DisplayHook $hook
+	 */
+	public function processEdit(Zikula_DisplayHook $hook)
+	{
+		$view = new Zikula_Request_Http();
+		$albumyes = $view->getPost()->filter('muimage-albumyes', '');
+
+		$files = News_ImageUtil::reArrayFiles(FormUtil::getPassedValue('news_files', null, 'FILES'));
+		// if the user has selected images for upload
+		if ($files) {
+			// if the user want to create a new muimage album
+			if ($albumyes != '') {
+				
+				$serviceManager = ServiceUtil::getManager();
+				$entityManager = $serviceManager->getService('doctrine.entitymanager');
+				
+				$story = FormUtil::getPassedValue('story', isset($args['story']) ? $args['story'] : null, 'POST');
+				$newstitle = $story['title'];
+				
+				LogUtil::registerStatus('toll');
+				$album = new MUImage_Entity_Album();
+
+				$uid = UserUtil::getVar('uid');
+				$album->setCreatedUserId($uid);
+				$album->setUpdatedUserId($uid);
+				
+				$date = new DateTime("now");
+
+				$album->setCreatedDate($date);
+				$album->setUpdatedDate($date);
+				
+				$album->setTitle(DateUtil::formatDatetime($date, 'datebrief') . ' - ' . $newstitle);
+
+				$entityManager->persist($album);
+				$entityManager->flush();
+			}
+			else {
+				
+			}
+		}
+		else {
+			// nothing to do
+		}
+		 
+	}
+}
---
+++ src/modules/MUImage/templates/hook/edit.tpl
@@ -0,0 +1,25 @@
+<fieldset>
+    <legend>{gt text="Create MUImage Album"}</legend>
+    <div class="muimage-hook z-formrow">
+        <label for="muimage-albumyes">{gt text='Create Album and add the uploaded pictures?'}</label>
+        <input type="checkbox" id="muimage-albumyes" name="muimage-albumyes" />
+    </div>
+        <div class="muimage-hook z-formrow">
+        <label for="muimage-album">{gt text='Add pictures to main album:'}</label>
+        <select id="muimage-album" name="muimage-album" />
+            <option value="">{gt text='Select a main album'}</option>
+            {foreach item='album' from=$albums}
+                <option value={$album.id}>{$album.title}</option>
+            {/foreach}
+        </select>
+    </div>  
+        <div class="muimage-hook z-formrow">
+        <label for="muimage-subalbum">{gt text='Add pictures to sub album:'}</label>
+        <select id="muimage-subalbum" name="muimage-subalbum" />
+            <option value="">{gt text='Select a sub album'}</option>
+            {foreach item='subalbum' from=$subalbums}
+                <option value={$album.id}>{$subalbum.title}</option>
+            {/foreach}
+        </select>
+    </div> 
+</fieldset>
