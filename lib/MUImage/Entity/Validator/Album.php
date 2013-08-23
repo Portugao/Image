70446ff151cb84c89147827bfcd903891a21e1ee
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2013-08-23 17:47:56 +0200

    no class Validator/Album.php

--- lib/MUImage/Entity/Validator/Album.php
+++ lib/MUImage/Entity/Validator/Album.php
@@ -17,35 +17,6 @@
  * This is the concrete validation class for album entities.
  */
 class MUImage_Entity_Validator_Album extends MUImage_Entity_Validator_Base_Album
-{
-    
-    /**
-     * Performs all validation rules.
-     *
-     * @return mixed either array with error information or true on success
-     */
-    public function validateAll()
-    {
-        $errorInfo = array('message' => '', 'code' => 0, 'debugArray' => array());
-        $dom = ZLanguage::getModuleDomain('MUImage');
-        if (!$this->isStringNotLongerThan('title', 255)) {
-            $errorInfo['message'] = __f('Error! Length of field value must not be higher than %2$s (%1$s).', array('title', 255), $dom);
-            return $errorInfo;
-        }
-        if (!$this->isStringNotEmpty('title')) {
-            $errorInfo['message'] = __f('Error! Field value must not be empty (%s).', array('title'), $dom);
-            return $errorInfo;
-        }
-        if (!$this->isStringNotLongerThan('description', 2000)) {
-            $errorInfo['message'] = __f('Error! Length of field value must not be higher than %2$s (%1$s).', array('description', 2000), $dom);
-            return $errorInfo;
-        }
-        if (!$this->isUniqueValue('title')) {
-            $errorInfo['message'] = __f('The title %s is already assigned. Please choose another title.', array('title'), $dom);
-            return $errorInfo;
-        }
-        return true;
-    }
-    
-
+{    
+    // feel free to extend the ipmlement own methods
 }
