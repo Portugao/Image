4698ccbf3f405ad84a12dfc5613588813e7dc925
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2013-12-30 19:44:25 +0100

    typo

--- lib/MUImage/Version.php
+++ lib/MUImage/Version.php
@@ -30,7 +30,7 @@ class MUImage_Version extends MUImage_Base_Version
         $meta['url'] = $this->__('muimage');
         // core requirement
         $meta['core_min'] = '1.3.1'; // requires minimum 1.3.1 or later
-        $meta['core_max'] = '1.3.5'; // not ready for 1.3.6 yet
+        $meta['core_max'] = '1.3.6'; // not ready for 1.3.7 yet
 
         // define special capabilities of this module
         $meta['capabilities'] = array(
