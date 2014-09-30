7ace699acd2052f2b0747cd4733724db320922f3
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2014-09-30 23:26:51 +0200

    implemented

--- lib/MUImage/Util/ListEntries.php
+++ lib/MUImage/Util/ListEntries.php
@@ -16,5 +16,40 @@
  */
 class MUImage_Util_ListEntries extends MUImage_Util_Base_ListEntries
 {
-    // feel free to add your own convenience methods here
+    /**
+     * Get 'album access' list entries.
+     *
+     * @return array Array with desired list entries.
+     */
+    public function getAlbumAccessEntriesForAlbum()
+    {
+        $states = array();
+        $states[] = array('value'   => 'all',
+                          'text'    => $this->__('All'),
+                          'title'   => '',
+                          'image'   => '',
+                          'default' => false);
+        $states[] = array('value'   => 'users',
+                          'text'    => $this->__('Users'),
+                          'title'   => $this->__('only registered users'),
+                          'image'   => '',
+                          'default' => false);
+        $states[] = array('value'   => 'friends',
+                          'text'    => $this->__('Friends'),
+                          'title'   => '',
+                          'image'   => '',
+                          'default' => false);
+        $states[] = array('value'   => 'known',
+                          'text'    => $this->__('Known password'),
+                          'title'   => '',
+                          'image'   => '',
+                          'default' => false);
+       /* $states[] = array('value'   => 'forme',
+                          'text'    => $this->__('Only for me'),
+                          'title'   => '',
+                          'image'   => '',
+                          'default' => false);*/
+    
+        return $states;
+    }
 }
