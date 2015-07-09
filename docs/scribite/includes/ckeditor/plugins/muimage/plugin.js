62f800eb729f27e117dbac85c4016f049a2459a2
Thomas Richers <webdesign@thomas-richers.de>
2015-07-09 11:34:55 +0200

    Update plugin.js
    Ich finde es hilfreich, wenn man gleich in die Ansicht der Bilder kommt.

--- docs/scribite/includes/ckeditor/plugins/muimage/plugin.js
+++ docs/scribite/includes/ckeditor/plugins/muimage/plugin.js
@@ -4,7 +4,7 @@ CKEDITOR.plugins.add('MUImage', {
     init: function (editor) {
         editor.addCommand('insertMUImage', {
             exec: function (editor) {
-                var url = Zikula.Config.baseURL + Zikula.Config.entrypoint + '?module=MUImage&type=external&func=finder&editor=ckeditor';
+                var url = Zikula.Config.baseURL + Zikula.Config.entrypoint + '?module=MUImage&type=external&func=finder&editor=ckeditor&album=0';
                 // call method in MUImage_Finder.js and also give current editor
                 MUImageFinderCKEditor(editor, url);
             }
