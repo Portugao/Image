89ecca3e7c81b8a47e70fa82cc33df003ae5fdbb
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2017-01-10 22:27:15 +0100

    link for create albums

--- Container/Base/AbstractLinkContainer.php
+++ Container/Base/AbstractLinkContainer.php
@@ -160,7 +160,15 @@ abstract class AbstractLinkContainer implements LinkContainerInterface
                 'title' => $this->__('Album list')
             ];
         }
-        if (in_array('picture', $allowedObjectTypes)
+        if (in_array('album', $allowedObjectTypes)
+        		&& $this->permissionApi->hasPermission($this->getBundleName() . ':Album:', '::', ACCESS_EDIT)) {
+        			$links[] = [
+        					'url' => $this->router->generate('muimagemodule_album_' . $routeArea . 'edit'),
+        					'text' => $this->__('New album'),
+        					'title' => $this->__('Create new album')
+        			];
+        		}
+        /*if (in_array('picture', $allowedObjectTypes)
             && $this->permissionApi->hasPermission($this->getBundleName() . ':Picture:', '::', $permLevel)) {
             $links[] = [
                 'url' => $this->router->generate('muimagemodule_picture_' . $routeArea . 'view'),
@@ -168,7 +176,7 @@ abstract class AbstractLinkContainer implements LinkContainerInterface
                 'title' => $this->__('Picture list')
             ];
         }
-        /*if (in_array('avatar', $allowedObjectTypes)
+        if (in_array('avatar', $allowedObjectTypes)
             && $this->permissionApi->hasPermission($this->getBundleName() . ':Avatar:', '::', $permLevel)) {
             $links[] = [
                 'url' => $this->router->generate('muimagemodule_avatar_' . $routeArea . 'view'),
